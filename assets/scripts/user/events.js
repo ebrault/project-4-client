20ccf938a52a6ddee9f8c0d91e552871594c4678
ebrault <emmet.brault@gmail.com>
2018-11-30 16:17:08 -0500

    Built client for auth
    * Refactored browser template for auth with API back end
    * Added Sign Up, Sign In, Change Password forms, Sign Out button using
    Get Form Fields to index.html
    * Added hidden class element to CSS
    * Added Event Handlers to Auth Events in app.js
    * Added Events to events.js
    * Added API requests to api.js
    * Added UI responses to ui.js
    * Ran servers and tested auth communication between front and back end
    successfully

---
+++ assets/scripts/user/events.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const getFormFields = require('../../../lib/get-form-fields.js')
+const api = require('./api.js')
+const ui = require('./ui.js')
+const store = require('../store.js')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+  store.user = {}
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut
+}
---
+++ assets/scripts/user/ui.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const store = require('../store.js')
+
+const signUpSuccess = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Sign Up Successful!')
+  $('#display-message').css('color', 'green')
+  $('#sign-up-form').trigger('reset')
+}
+
+const signUpFailure = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#sign-up-form').trigger('reset')
+}
+
+const signInSuccess = function (response) {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Sign In Successful!')
+  $('#display-message').css('color', 'green')
+  $('#sign-in-form').trigger('reset')
+  store.user = response.user
+  $('#sign-up-form').addClass('hidden')
+  $('#sign-in-form').addClass('hidden')
+  $('#change-password-form').removeClass('hidden')
+  $('#sign-out-button').removeClass('hidden')
+}
+
+const signInFailure = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#sign-in-form').trigger('reset')
+}
+
+const changePasswordSuccess = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Password changed successfully!')
+  $('#display-message').css('color', 'green')
+  $('#change-password-form').trigger('reset')
+}
+
+const changePasswordFailure = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#change-password-form').trigger('reset')
+}
+
+const signOutSuccess = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Sign out successful!')
+  $('#display-message').css('color', 'green')
+  $('#change-password-form').trigger('reset')
+  $('#change-password-form').addClass('hidden')
+  $('#sign-up-form').removeClass('hidden')
+  $('#sign-in-form').removeClass('hidden')
+}
+
+const signOutFailure = function () {
+  $('#display-message').removeClass('hidden')
+  $('#display-message').html('Something went wrong, please try again!')
+  $('#display-message').css('color', 'red')
+  $('#change-password-form').trigger('reset')
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
